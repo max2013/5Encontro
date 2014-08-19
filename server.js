0c36d7e0bf12f33f3b0302d24ad4bdc07d05412c
Rafael Alves <rafael@maxexperience.com.br>
2014-08-19 13:09:31 -0300

    app

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000, '192.168.0.142');
+});
+
+
+console.log('Server running at http://192.168.0.142/');
